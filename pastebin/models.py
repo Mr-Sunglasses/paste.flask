291288b1f3e4112c2eef33acd510cbb320342cc8
Kanishk Pachauri <krishna03012003@icloud.com>
2023-01-19 04:55:45 +0530

    Defined Model and Dockerfile 🎣

--- pastebin/models.py
+++ pastebin/models.py
@@ -1,2 +1,14 @@
 from pastebin import db
 
+class Bin(db.Model):
+
+    __tablename__ = "pastebinpys"
+
+    id: int = db.Column(db.Integer, primary_key = True )
+    content: str = db.Column(db.Text)
+
+    def __init__(self, content) -> None:
+        self.content = content
+    
+    def __repr__(self) -> str:
+        return f"{self.content}"
